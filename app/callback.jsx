12dab369973b3feeba44f9248a48e1034c5ee234
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2025-09-27 15:31:56 -0700

    fix mobile spotify auth

--- app/callback.jsx
+++ app/callback.jsx
@@ -1,14 +1,25 @@
-
-
+import { useEffect } from 'react';
 import { handleSpotifyCallback } from '../services/spotify';
+import { Linking, Platform } from 'react-native';
 
-export default function callback(event, context) {
-    // web only page
-    const currentUrl = window.location.href;
-    handleSpotifyCallback(currentUrl);//, () => { window.location.href = '/' });
+export default function Callback() {
+    useEffect(() => {
+        if (Platform.OS === 'web') {
+            const currentUrl = window.location.href;
+            handleSpotifyCallback(currentUrl);
+        } else {
+            // mobile
+            Linking.getInitialURL().then(url => {
+                if (url) handleSpotifyCallback(url);
+            });
 
-    return (
-        <>
-        </>
-    )
-};
+            const subscription = Linking.addEventListener('url', event => {
+                handleSpotifyCallback(event.url);
+            });
+
+            return () => subscription.remove();
+        }
+    }, []);
+
+    return <></>;
+}
