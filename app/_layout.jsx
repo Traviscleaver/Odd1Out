d4b3bcc13918963f53226dec468423ef39014a4a
Traviscleaver <151422711+Traviscleaver@users.noreply.github.com>
2025-09-27 16:28:05 -0700

    Remove NAV Bar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="game" options={{ headerShown: false }} />
+      <Stack.Screen name="join" options={{ headerShown: false }} />
+      <Stack.Screen name="lobby" options={{ headerShown: false }} />
+      <Stack.Screen name="play" options={{ headerShown: false }} />
+      <Stack.Screen name="settings" options={{ headerShown: false }} />
+    </Stack>;
 }
 
 
